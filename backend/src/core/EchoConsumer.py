c48ed4ee5bbd7b3dce43d9901c59b7716b55b1cc
pyrec-dev <pyrec-dev@pyrec-dev.pyrec-dev>
2025-10-13 03:40:09 -0400

    Consumer : Basic Layout

--- backend/src/core/EchoConsumer.py
+++ backend/src/core/EchoConsumer.py
@@ -1,14 +1,14 @@
 #Basic Layout
-from channels.consumer import SyncConsumer
+from channels.consumer import AsyncConsumer, SyncConsumer
 
 
 class EchoConsumer(SyncConsumer):
-    def websocket_connect(self, event):
+    def websocket_connect(self, event): # websocket.connect
         self.send({
             "type": "websocket.accept"
         })
 
-    def websocket_receive(self, event):
+    def websocket_receive(self, event): # websocket.receive
         message = event.get('text', '')
         self.send({
             "type": "websocket.send",
@@ -16,4 +16,21 @@ class EchoConsumer(SyncConsumer):
         })
 
     def websocket_disconnect(self, event):
+        pass
+
+
+class AsyncEchoConsumer(AsyncConsumer):
+    async def websocket_connect(self, event): # websocket.connect
+        await self.send({
+            "type": "websocket.accept"
+        })
+
+    async def websocket_receive(self, event): # websocket.receive
+        message = event.get('text', '')
+        await self.send({
+            "type": "websocket.send",
+            "text": message
+        })
+
+    async def websocket_disconnect(self, event):
         pass
--- backend/src/src/asgi.py
+++ backend/src/src/asgi.py
@@ -9,13 +9,24 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 
 import os
 
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
+from django.urls import re_path
+
+from core.EchoConsumer import EchoConsumer
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.settings')
 django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
-    # Just HTTP for now. (We can add other protocols later.)
+    "wbsocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter([
+                re_path(r"ws/echo/$", EchoConsumer.as_asgi()),
+            ])
+        )
+    ),
 })
